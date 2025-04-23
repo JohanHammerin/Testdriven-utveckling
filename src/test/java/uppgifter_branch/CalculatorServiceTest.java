b2b6eef9e78079ee00a5edd75c28879b082bdc09
johanhammerin <johan04hammerin@gmail.com>
2025-04-23 18:22:15 +0200

    add new test

--- src/test/java/uppgifter_branch/CalculatorServiceTest.java
+++ src/test/java/uppgifter_branch/CalculatorServiceTest.java
@@ -1,6 +1,26 @@
 package uppgifter_branch;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 class CalculatorServiceTest {
-  
+
+    @Test
+    @DisplayName(value = "Summan av addition mellan två tal")
+    void addingTwoNumbers_returnsCorrectSum() {
+        CalculatorService calculatorService = new CalculatorService();
+        calculatorService.firstNum = 3;
+        calculatorService.secondNum = 7;
+        assertEquals(10, calculatorService.integerSum());
+    }
+
+    @Test
+    @DisplayName(value = "Summan av addition mellan två positiva tal")
+    void addingTwoPositiveNumbers_returnsCorrectSum() {
+        CalculatorService calculatorService = new CalculatorService();
+        calculatorService.firstNum = 10;
+        calculatorService.secondNum = -20;
+        assertEquals(3.1415926535, calculatorService.positiveIntegerSum());
+    }
 }
